48aeec0c8225cbb718e55a908fce97fefdd561d0
Honghui Xu <18482292781@163.com>
2026-08-08 15:55:22 -0400

    Lesson 02: Variables and Object Identity

---
+++ 01_python/lesson01_environment/hello.py
@@ -0,0 +1,2 @@
+print("Hello, AI System!")
+print("I am building AI systems.")
---
+++ 01_python/lesson02_variables/variable.py
@@ -0,0 +1,17 @@
+# name = "Honghui"
+
+# print(name)
+# print(id(name))
+
+
+# name = "Dr. Xu"
+
+# print(name)
+# print(id(name))
+
+a = 10
+
+b = a
+
+print(id(a))
+print(id(b))
